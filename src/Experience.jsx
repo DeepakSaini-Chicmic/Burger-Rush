cb532cc6430d965a9377690ee7b9343d6fb464da
Deepak-Chicmic <deepak.saini@chicmic.co.in>
2023-06-28 18:29:41 +0530

    Player Component created

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,15 +4,17 @@ import { Perf } from "r3f-perf";
 import Lights from "./Lights.jsx";
 import Level from "./Level.jsx";
 import { BlockSpinner, BlockAxe, BlockLimbo } from "./Level.jsx";
+import Player from "./Player.jsx";
 
 export default function Experience() {
   return (
     <>
       <Perf position="top-left" />
       <OrbitControls makeDefault />
-      <Physics>
+      <Physics debug>
         <Lights />
         <Level count={5} types={[BlockSpinner, BlockAxe, BlockLimbo]} />
+        <Player />
       </Physics>
     </>
   );
